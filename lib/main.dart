fb605ba55ff02ad80340d9e6e46b37cb9adc07d7
Nursafika97 <nursafikaefendi@gmail.com>
2025-04-21 20:32:59 +0700

    main

--- lib/main.dart
+++ lib/main.dart
@@ -7,64 +7,28 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'action Button App',
+      title: 'Text Widget App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const Home(),
-    );
-  }
-}
-
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
-
-  @override
-  HomeState createState() => HomeState();
-}
-
-class HomeState extends State<Home> {
-  String fileName = 'images/logo_kelas.png';
-
-  void selectImage(int index) {
-    setState(() {
-      switch (index) {
-        case 0:
-          fileName = 'images/logo_kelas.png';
-          break;
-        case 1:
-          fileName = 'images/POLBENG.png';
-          break;
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Demo Action Button'),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.laptop),
-            onPressed: () {
-              selectImage(0);
-            },
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Demo Text Widget')),
+        body: Center(
+          child: Text(
+            'Rekayasa Perangkat Lunak',
+            textDirection: TextDirection.ltr,
+            style: const TextStyle(
+              fontSize: 30.0,
+              fontStyle: FontStyle.italic,
+              color: Colors.red,
+            ),
           ),
-          IconButton(
-            icon: const Icon(Icons.account_balance),
-            onPressed: () {
-              selectImage(1);
-            },
-          ),
-        ],
-      ),
-      body: ListView(
-        children: <Widget>[Image.asset(fileName, height: 350.0, width: 350.0)],
+        ),
       ),
     );
   }
